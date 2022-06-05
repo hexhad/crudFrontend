32434bae642a799c78e6fd86f44f77b190936821
hexhad <hexhad@gmail.com>
2022-06-04 23:02:25 -0700

    error control fetch

--- src/services/index.js
+++ src/services/index.js
@@ -8,13 +8,13 @@ const headers = {
 export const getAllTodos = async () => {
   return await axios.get("http://127.0.0.1:8081/api/task/read-all", {
     headers,
-  });
+  }).catch((e)=>{console.log("getAllTodos::ERR "+e)});
 };
 
 export const getOneTodo = async (itemId) => {
   return await axios.get("http://127.0.0.1:8081/api/task/read-one/" + itemId, {
     headers,
-  });
+  }).catch((e)=>{console.log("getOneTodo::ERR "+e)});
 };
 
 export const createTodo = async (desc) => {
@@ -22,7 +22,7 @@ export const createTodo = async (desc) => {
     headers,
     taskDetails: desc,
     status: "ON_GOING",
-  });
+  }).catch((e)=>{console.log("createTodo::ERR "+e)});
 };
 
 export const updateTodo = async (itemId, desc,status) => {
@@ -31,11 +31,11 @@ export const updateTodo = async (itemId, desc,status) => {
     taskId: itemId,
     taskDetails: desc,
     status: status
-  });
+  }).catch((e)=>{console.log("updateTodo::ERR "+e)});
 };
 
 export const deleteTodo = async (itemId) => {
   return await axios.delete("http://127.0.0.1:8081/api/task/delete/" + itemId, {
     headers,
-  });
+  }).catch((e)=>{console.log("deleteTodo::ERR "+e)});
 };
